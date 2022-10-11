3b4b325751d610f6f13e4d7cfeb7732a961fdf33
Emma Roskopf <79764856+eroskopf@users.noreply.github.com>
2022-10-11 23:00:55 +0200

    Create intro_datasets.py

---
+++ analysis/intro_datasets.py
@@ -0,0 +1,11 @@
+'''
+Author: Em
+Date: 10-11-22
+Purpose: To provide some insight into the data that we currently have and start to visualize it
+'''
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#need to import the files
